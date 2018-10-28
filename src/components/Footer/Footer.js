8906c2906b7aaf003c845a3e53d7c72c4e63dcd0
Gaurav Jayasawal <jgaurav6@gmail.com>
2018-10-28 04:14:25 -0400

    footer complete

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,28 +6,57 @@ class Footer extends Component {
     return (
       <div className="footer-container">
         <div className="footer-row-1">
-          <div className="footer-logo">PSU Corders.</div>
+          <div className="footer-logo">
+            PSU Coders <span className="greenDot">.</span>
+          </div>
           <div className="footer-navigation">
-            <div className="footer-navigation-item">HOME</div>
-            <div className="footer-navigation-item">BLOG</div>
-            <div className="footer-navigation-item">CONTACT</div>
-            <div className="footer-navigation-item">ABOUT</div>
+            <div className="footer-navigation-item">
+              {" "}
+              <a href="">HOME</a>{" "}
+            </div>
+            <div className="footer-navigation-item">
+              <a href="">BLOG</a>
+            </div>
+            <div className="footer-navigation-item">
+              <a href="">CONTACT</a>
+            </div>
+            <div className="footer-navigation-item">
+              <a href="">ABOUT</a>
+            </div>
           </div>
           <div className="footer-social">
-            <div className="footer-social-item">Be</div>
-            <div className="footer-social-item">M</div>
-            <div className="footer-social-item">Ba</div>
+            <div className="footer-social-item">
+              <a href="">
+                <i className="fab fa-instagram" />
+              </a>
+            </div>
+            <div className="footer-social-item">
+              <a href="">
+                <i className="fab fa-twitter" />
+              </a>
+            </div>
+            <div className="footer-social-item">
+              <a href="">
+                <i className="fab fa-facebook" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="footer-row-2">
-          <hr />
+          <hr id="footer-hr" />
         </div>
         <div className="footer-row-3">
           <div className="footer-rights">
             © 2018 Creation All Rights Reserved
           </div>
-          <div className="footer-privacy-policy">PRIVACY POLICY</div>
-          <div className="footer-terms-of-service">TERMS OF SERVICE</div>
+          <div className="terms-and-privacy">
+            <div className="footer-privacy-policy">
+              <a href="">PRIVACY POLICY</a>
+            </div>
+            <div className="footer-terms-of-service">
+              <a href="">TERMS OF SERVICE</a>
+            </div>
+          </div>
         </div>
       </div>
     );
