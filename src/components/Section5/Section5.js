aed352216056ff09a0a86e3e2b56b6f780b88988
Gaurav Jayasawal <jgaurav6@gmail.com>
2018-11-09 17:39:40 -0500

    secretary card removed

--- src/components/Section5/Section5.js
+++ src/components/Section5/Section5.js
@@ -43,22 +43,11 @@ class Section5 extends Component {
       details: "Coding and Design Lord of Plattsburgh"
     },
     {
-      imageUrl:
-        "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/39119382_2428377430522427_2570412788488863744_n.jpg?_nc_cat=110&_nc_ht=scontent-lga3-1.xx&oh=a6b2b89b4a49acae343bebe6f2d9cc38&oe=5C7E3BC0",
+      imageUrl: "https://avatars2.githubusercontent.com/u/27921089?v=4",
       name: "Keith Prevost",
       title: "PR CHAIR",
       email: "mailto:kprev003@plattsburgh.edu",
       linkedIn: "",
-      website: "web",
-      details: "Coding and Design Lord of Plattsburgh"
-    },
-    {
-      imageUrl:
-        "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/28468608_1514372138679185_8429054350566895709_n.jpg?_nc_cat=111&_nc_ht=scontent-lga3-1.xx&oh=9d4ea9a472d96f5bee245da18e246678&oe=5C86BC73",
-      name: "Neel Adalja",
-      title: "Secretary",
-      email: "mailto:@plattsburgh.edu",
-      linkedIn: "https://www.linkedin.com/in/neel-adalja-731a2a153/",
       website: "web",
       details: "Coding and Design Lord of Plattsburgh"
     }
