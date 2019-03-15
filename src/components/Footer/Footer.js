c2ac0ee5dacede26eb694522c4681439f2b3bbb2
Swornim Barahi <sbara002@plattsburgh.edu>
2019-03-15 10:22:24 -0400

    Finished footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,63 +1,48 @@
 import React, { Component } from "react";
 import "./Footer.css";
 
+import LogoImage from '../../main_assets/images/coding_hub_ic.svg';
+
+import Github from '../../main_assets/images/github.svg';
+import Gmail from '../../main_assets/images/gmail_color_ic.svg';
+import Instagram from '../../main_assets/images/instagram.svg';
+import LinkedIn from '../../main_assets/images/linkedin.svg';
+
 class Footer extends Component {
   render() {
     return (
       <div className="footer-container">
         <div className="footer-row-1">
-          <div className="footer-logo">
-            Coding Hub
-            <span className="greenDot">.</span>
+          <div className="footer-logo-container">
+            <img src={ LogoImage } alt="" className="footer-logo"/>
+            <span>Coding Hub</span>
           </div>
           <div className="footer-navigation">
             <div className="footer-navigation-item">
-              {" "}
-              <a href="/">HOME</a>{" "}
-            </div>
-            <div className="footer-navigation-item">
-              <a href="/">BLOG</a>
-            </div>
-            <div className="footer-navigation-item">
-              <a href="/">CONTACT</a>
-            </div>
-            <div className="footer-navigation-item">
               <a href="/">ABOUT</a>
             </div>
+            <div className="footer-navigation-item">
+              <a href="/">TEAM</a>
+            </div>
+            <div className="footer-navigation-item">
+              <a href="/">SERVICES</a>
+            </div>
+            <div className="footer-navigation-item">
+              <a href="/">PROJECTS</a>
+            </div>
+            <div className="footer-navigation-item">
+              <a href="/">CONTACT US</a>
+            </div>
           </div>
-          <div className="footer-social">
-            <div className="footer-social-item">
-              <a href="/">
-                <i className="fab fa-instagram" />
-              </a>
-            </div>
-            <div className="footer-social-item">
-              <a href="/">
-                <i className="fab fa-twitter" />
-              </a>
-            </div>
-            <div className="footer-social-item">
-              <a href="/">
-                <i className="fab fa-facebook" />
-              </a>
-            </div>
+          <div className="social-icons">
+            <img src={ Gmail } alt=""/>
+            <img src={ LinkedIn } alt=""/>
+            <img src={ Github } alt=""/>
+            <img src={ Instagram } alt=""/>
           </div>
         </div>
         <div className="footer-row-2">
-          <hr id="footer-hr" />
-        </div>
-        <div className="footer-row-3">
-          <div className="footer-rights">
-            © 2018 Creation All Rights Reserved
-          </div>
-          <div className="terms-and-privacy">
-            <div className="footer-privacy-policy">
-              <a href="/">PRIVACY POLICY</a>
-            </div>
-            <div className="footer-terms-of-service">
-              <a href="/">TERMS OF SERVICE</a>
-            </div>
-          </div>
+          <span className="copyright">&copy; 2018 Coding Hub All Rights Reserved</span>
         </div>
       </div>
     );
