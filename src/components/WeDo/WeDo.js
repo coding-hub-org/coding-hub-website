9bf63a7ca7a0aedffebcdd3650f01aa39452f847
Swornim Barahi <sbara002@plattsburgh.edu>
2019-03-21 12:49:47 -0400

    Added data_icon and responsiveness

--- src/components/WeDo/WeDo.js
+++ src/components/WeDo/WeDo.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DataIco from "../../main_assets/images/data_icon.svg";
 import "./WeDo.css";
 
 const WeDo = props => {
@@ -8,10 +9,15 @@ const WeDo = props => {
         <div className="WeDo-Col">
           <h2 className = "service-title-1">{ props.info[2] }</h2>
           <h2 className = "service-title-2">{ props.info[3] }</h2>
-          <p className = "service-desc">{ props.info[4] }</p>
+          <div className = "service-row-3">
+            <img src={ DataIco } alt="" className = "data-icon"/>
+            <div className = "service-desc">
+              { props.info[4] }
+            </div>
+          </div>
         </div>
         <div className="WeDo-Col">
-          <img src={ props.info[1] } alt="" className = "pic"/>
+          <img src={ props.info[1] } alt="" className = "WeDo-pic"/>
         </div>
       </div>
     );
@@ -20,12 +26,17 @@ const WeDo = props => {
     return(
       <div className = "WeDo-container">
         <div className="WeDo-Col">
-          <img src={ props.info[1] } alt="" className = "pic"/>
+          <img src={ props.info[1] } alt="" className = "WeDo-pic"/>
         </div>
         <div className="WeDo-Col">
           <h2 className = "service-title-1">{ props.info[2] }</h2>
           <h2 className = "service-title-2">{ props.info[3] }</h2>
-          <p>{ props.info[4] }</p>
+          <div className = "service-row-3">
+            <img src={ DataIco } alt="" className = "data-icon"/>
+            <div className = "service-desc">
+              { props.info[4] }
+            </div>
+          </div>
         </div>
       </div>
     )
