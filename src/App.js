0717b58fbb85fea690029a7baf09f2adbc42542f
votrungquan1999 <32785143+votrungquan1999@users.noreply.github.com>
2019-03-26 09:49:07 -0400

    landing page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
-// import About from "./components/About/About";
-import Section2 from "./components/Section2/Section2";
-import Section3 from "./components/Section3/Section3";
-import Section4 from "./components/Section4/Section4";
-import Section5 from "./components/Section5/Section5";
-import Section6 from "./components/Section6/Section6";
-import Footer from "./components/Footer/Footer";
-import Section1 from "./components/Section1/Section1";
+
+import Home from './components/v2/Home/Home'
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Section1 />
-        <Section2 />
-        <Section3 />
-        <Section4 />
-        <Section5 />
-        <Section6 />
-        <Footer />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Home/>
+			</div>
+		);
+	}
 }
 
 export default App;
