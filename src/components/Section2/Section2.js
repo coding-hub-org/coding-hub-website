a29119dc031eca0da58a9c5a2b95e1a24c2b2d15
astrahl008 <astra008@plattsburgh.edu>
2019-04-04 18:46:01 -0400

    Updated the about section

--- src/components/Section2/Section2.js
+++ src/components/Section2/Section2.js
@@ -4,7 +4,7 @@ import About from "../About/About";
 import ScrollableAnchor from "react-scrollable-anchor";
 
 let aboutUs =
-  "      Coding Hub was co-founded in 2018 to develop mobile and web applications to provide services to students on the SUNY Plattsburgh campus. The main mission of the club is to make the lives of students easier with the use of software products.";
+  "Coding Hub was co-founded in 2018 to develop mobile and web applications to provide services to students on the SUNY Plattsburgh campus. The main mission of the club is to make the lives of students easier with the use of software products.";
 
 const section2 = props => (
   <ScrollableAnchor id={"About"}>
