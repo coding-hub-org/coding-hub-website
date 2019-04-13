09bc5f4046305e069f63a5e600610cdfdd2228e9
gonzen2310 <gonzaloreyesllauca@gmail.com>
2019-04-13 00:49:39 -0400

    Clened up project files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-// import About from "./components/About/About";
-// import Section2 from "./components/Section2/Section2";
-// import Section3 from "./components/Section3/Section3";
-// import Section3End from "./components/Section3/Section3End";
-// import Section4 from "./components/Section4/Section4";
-// import Section6 from "./components/Section6/Section6";
+
 // import ProjectSection from "./sections/ProjectsSection/ProjectsSection";
 
 import HomeSection from "./sections/HomeSection/HomeSection";
@@ -19,14 +14,7 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				{/* <Home/>
-        <Section2 />
-        <Section3 />
-        <Section3End /> */}
-				{/* <Section4 /> */}
-				{/* <Section5 /> */}
-				{/* <ProjectSection/> 
-        <Section6 /> */}
+				{/* <ProjectSection/>*/}
 				<HomeSection />
 				<AboutSection />
 				<WhatWeDoSection />
