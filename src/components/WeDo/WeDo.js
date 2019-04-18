60d6f03420ae2945e8b2e990c7434d600abfa4cd
Swornim Barahi <sbara002@plattsburgh.edu>
2019-04-18 18:40:01 -0400

    Resize the whatwedo section

--- src/components/WeDo/WeDo.js
+++ src/components/WeDo/WeDo.js
@@ -5,42 +5,37 @@ import "./WeDo.css";
 const WeDo = props => {
   if (props.info[0] === "right") {
     return (
-      <div className = "WeDo-container">
-        <div className="WeDo-Col">
-          <h2 className = "service-title-1">{ props.info[2] }</h2>
-          <h2 className = "service-title-2">{ props.info[3] }</h2>
-          <div className = "service-row-3">
-            <img src={ DataIco } alt="" className = "data-icon"/>
-            <div className = "service-desc">
-              { props.info[4] }
-            </div>
+      <div className="WeDo-container">
+        <div className="WeDo-Col WeDo-Col-Fix">
+          <h2 className="service-title-1">{props.info[2]}</h2>
+          <h2 className="service-title-2">{props.info[3]}</h2>
+          <div className="service-row-3">
+            <img src={DataIco} alt="" className="data-icon" />
+            <div className="service-desc">{props.info[4]}</div>
           </div>
         </div>
         <div className="WeDo-Col">
-          <img src={ props.info[1] } alt="" className = "WeDo-pic"/>
+          <img src={props.info[1]} alt="" className="WeDo-pic" />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="WeDo-container">
+        <div className="WeDo-Col">
+          <img src={props.info[1]} alt="" className="WeDo-pic" />
+        </div>
+        <div className="WeDo-Col WeDo-Col-Fix">
+          <h2 className="service-title-1">{props.info[2]}</h2>
+          <h2 className="service-title-2">{props.info[3]}</h2>
+          <div className="service-row-3">
+            <img src={DataIco} alt="" className="data-icon" />
+            <div className="service-desc">{props.info[4]}</div>
+          </div>
         </div>
       </div>
     );
   }
-  else {
-    return(
-      <div className = "WeDo-container">
-        <div className="WeDo-Col">
-          <img src={ props.info[1] } alt="" className = "WeDo-pic"/>
-        </div>
-        <div className="WeDo-Col">
-          <h2 className = "service-title-1">{ props.info[2] }</h2>
-          <h2 className = "service-title-2">{ props.info[3] }</h2>
-          <div className = "service-row-3">
-            <img src={ DataIco } alt="" className = "data-icon"/>
-            <div className = "service-desc">
-              { props.info[4] }
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
+};
 
 export default WeDo;
