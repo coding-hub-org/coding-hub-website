2f8275c2e7786849c50a0a5a63c7bdbb6ae4781f
swornimbarahi <sbara002@plattsburgh.edu>
2019-06-24 22:46:53 -0700

    Changed App into a functional component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
-import React, { Component } from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-import ProjectSection from "./sections/ProjectsSection/ProjectsSection";
+import ProjectSection from './sections/ProjectsSection/ProjectsSection';
 
-import HomeSection from "./sections/HomeSection/HomeSection";
-import AboutSection from "./sections/AboutSection/AboutSection";
-import TeamSection from "./sections/TeamSection/TeamSection";
-import Footer from "./components/Footer";
-import ContactSection from "./sections/ContactSection/ContactSection";
-import WhatWeDoSection from "./sections/WhatWeDoSection/WhatWeDoSection";
+import HomeSection from './sections/HomeSection/HomeSection';
+import AboutSection from './sections/AboutSection/AboutSection';
+import TeamSection from './sections/TeamSection/TeamSection';
+import Footer from './components/Footer';
+import ContactSection from './sections/ContactSection/ContactSection';
+import WhatWeDoSection from './sections/WhatWeDoSection/WhatWeDoSection';
 
-class App extends Component {
-	render() {
-		console.log("FINISHED SET UP");
-		return (
-			<div className="App">
-				<HomeSection />
-				<AboutSection />
-				<WhatWeDoSection />
-				<TeamSection />
-				<ProjectSection />
-				<ContactSection />
-				<Footer />
-			</div>
-		);
-	}
+function App() {
+	return (
+		<div className="App">
+			<HomeSection />
+			<AboutSection />
+			<WhatWeDoSection />
+			<TeamSection />
+			<ProjectSection />
+			<ContactSection />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
