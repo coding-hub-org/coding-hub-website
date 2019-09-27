8dbae829c0f9af8551140022a47f0dc6eb4e930b
winkhanh <giakhanhnk2000@gmail.com>
2019-09-27 15:07:55 -0400

    remove 2 unknown projects (#28)

--- src/sections/ProjectsSection/ProjectsSection.js
+++ src/sections/ProjectsSection/ProjectsSection.js
@@ -17,22 +17,11 @@ class ProjectSection extends Component {
 					"This application is developed to help hundreds of students on campus to track the location of the campus shuttle. It helps save student time by notifying them exactly when the shuttle reaches the location they specified in their preferences."
 			},
 			{
-				title: "Housing Locator",
-				color: "#FFC453",
-				description:
-					"This application is aimed to help students on and off campus to find housing. Instead of personally going to talk to individual house owners or resident halls, they can filter housing based on their preferences simplifying the process of finding a place to live."
-			},
-			{
 				title: "Club websites",
 				color: "#FF4099",
 				link: "https://coding-hub.com",
 				description:
 					"These are the websites that we plan to make for various clubs in our campus. This allows club to have a personal space for posting notices and advertise their events to their members."
-			},
-			{
-				title: "ArkAngel",
-				color: "#222B8E",
-				description: "The project plan is still in progress."
 			},
 			{
 				title: "Rate my tutor",
